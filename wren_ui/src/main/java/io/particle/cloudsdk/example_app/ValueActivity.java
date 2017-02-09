30665fe42a88a3cb470c4ba2cb4f795d9accd4c0
Praakrit Pradhan <pradhapt@mail.uc.edu>
2017-02-09 16:23:00 -0500

    Changed icon and app name

--- wren_ui/src/main/java/io/particle/cloudsdk/example_app/ValueActivity.java
+++ wren_ui/src/main/java/io/particle/cloudsdk/example_app/ValueActivity.java
@@ -5,9 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.List;
 
 import io.particle.android.sdk.cloud.ParticleCloud;
 import io.particle.android.sdk.cloud.ParticleCloudException;
@@ -20,12 +25,26 @@ public class ValueActivity extends AppCompatActivity {
     private static final String ARG_VALUE = "ARG_VALUE";
     private static final String ARG_DEVICEID = "ARG_DEVICEID";
 
+    final ParticleCloud cloud = ParticleCloud.get(this);
+
     private TextView tv;
+
+    TextView tv2;
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    List devices;
+    String[] test_strings = {
+            "test_1",
+            "test_2",
+            "test_3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_value);
+
+        listView = (ListView) findViewById(R.id.list_view);
+
         tv = (TextView) findViewById(R.id.value);
         tv.setText(String.valueOf(getIntent().getIntExtra(ARG_VALUE, 0)));
 
@@ -37,6 +56,7 @@ public class ValueActivity extends AppCompatActivity {
                 Async.executeAsync(ParticleCloud.get(ValueActivity.this), new Async.ApiWork<ParticleCloud, Object>() {
                     @Override
                     public Object callApi(ParticleCloud ParticleCloud) throws ParticleCloudException, IOException {
+                        List devices = ParticleCloud.getDevices();
                         ParticleDevice device = ParticleCloud.getDevice(getIntent().getStringExtra(ARG_DEVICEID));
                         Object variable;
                         try {
@@ -45,6 +65,7 @@ public class ValueActivity extends AppCompatActivity {
                             Toaster.l(ValueActivity.this, e.getMessage());
                             variable = -1;
                         }
+                        variable = devices.get(0);
                         return variable;
                     }
 
@@ -60,6 +81,19 @@ public class ValueActivity extends AppCompatActivity {
                 });
             }
         });
+
+
+
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,test_strings);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" is selected",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
     public static Intent buildIntent(Context ctx, Integer value, String deviceid) {
